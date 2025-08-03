ce2d46d8e4161e9ca1cc1d2285d151cc744c5811
clot27 <adityayadav11082@protonmail.com>
2025-08-03 19:47:04 +0530

    formatting

--- configuration/flake.nix
+++ configuration/flake.nix
@@ -4,11 +4,14 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-  outputs = {self, nixpkgs, ...}@inputs: {
-  nixosConfigurations.nixos-niri = nixpkgs.lib.nixosSystem {
-     modules = [ ./configuration.nix ];
-     specialArgs = {inherit inputs; };
-
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: {
+    nixosConfigurations.nixos-niri = nixpkgs.lib.nixosSystem {
+      modules = [./configuration.nix];
+      specialArgs = {inherit inputs;};
     };
   };
 }
