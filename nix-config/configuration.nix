ed83b9118c00b8d8ac89d88d8573c0c1c47947e8
clot27 <adityayadav11082@protonmail.com>
2025-09-01 19:48:40 +0530

    52 current  2025-09-01 19:48:14  25.05.20250831.b4c2c57  6.12.44                          *

--- nix-config/configuration.nix
+++ nix-config/configuration.nix
@@ -1,5 +1,5 @@
 # Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
+#  your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 {
   config,
@@ -115,7 +115,7 @@
     bottom
     fish
     git
-    # gitui
+    gitui
     helix # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     macchina
     wget
